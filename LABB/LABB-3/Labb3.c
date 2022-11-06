6783e0d45a50d79e77c5350152b148bbc5d4dd76
abooddaker <abooddaker@gmail.com>
2022-11-06 23:53:27 +0100

    .

--- LABB/LABB-3/Labb3.c
+++ LABB/LABB-3/Labb3.c
@@ -3,7 +3,6 @@
 #define WORDLENGTH 20
 #define MAX 3
 
-
 struct warehouse
 {
     char productName[WORDLENGTH];
@@ -12,65 +11,40 @@ struct warehouse
 };
 typedef struct warehouse Warehouse;
 
-void registerProduct(Warehouse itemList[], int *pNrOfProducts)
-{
-    int productNumber, productAmount;
-    char productName[WORDLENGTH];
-    while (productNumber != 0)
-    {
-        printf("Ange varunummer (0 f”r avslut): ");
-        scanf("%d%*c", &productNumber);
-        if(productNumber == 0)
-        break;
-        printf("Ange namn: ", productName);
-        scanf("%s%*c", productName);
-        printf("Ange saldo: ", productAmount);
-        scanf("%d%*c", &productAmount);
-        
-        (*pNrOfProducts++);
+Warehouse createProduct(char productName[], int productNumber, int productAmount);
+void registerProduct(Warehouse registry[], int *pNrOfProducts);
+void printProducts(Warehouse p);
+void printRegistery(Warehouse registry[], int nrOfProducts);
+void searchMenu(Warehouse registry[], int nrOfProducts);
+void searchProductNumber(Warehouse registry[], int nrOfProducts);
+void searchProductAmount(Warehouse registry[], int nrOfProducts);
 
-    } 
-}
 
-/* void regCars(Car reg[],int *pNrOfCars)
-{
-    char again[WORDLENGTH] = "ja", model[WORDLENGTH];
-    int year,milage;
-    while (strcmp(again, "ja") == 0)
-    { 
-        printf("Ange model: ");
-        scanf("%s%*c",model);
-        printf("Ange year: ");
-        scanf("%d%*c",&year);
-        printf("Ange milage: ");
-        scanf("%d%*c",&milage);
-        reg[*pNrOfCars] = createCar(model,year,milage);
-        (*pNrOfCars)++;
-        printf("Vill du fortsatta? (ja eller nej) ");
-        scanf("%s%*c",again);
-    }
-}
-*/
+
+
 
 int main()
 {
-    int menuChoice, nrOfProducts;
-    Warehouse itemList[MAX];
-    
+    int menuChoice, nrOfProducts = 0;
+    Warehouse itemList[MAX], registry[MAX];
+
     while (menuChoice != 7)
     {
-        printf("V„lj en meny val (1-7): ");
-        printf("1. Registrera nya varor \n 2. Skriva ut alla varor \n 3. Soka efter varor \n 4. Andra lagersaldot f”r varor \n 5. Sortera varor \n 6. Avregistrera varor \n 7. Avsluta programmet ");
+        printf("\nV„lj en meny val (1-7):\n ");
+        printf("\n 1. Registrera nya varor \n 2. Skriva ut alla varor \n 3. Soka efter varor \n 4. Andra lagersaldot f”r varor \n 5. Sortera varor \n 6. Avregistrera varor \n 7. Avsluta programmet\n");
         scanf("%d", &menuChoice);
 
         switch(menuChoice)
         {
             case 1:
                 registerProduct(itemList, &nrOfProducts);
+                break;
             case 2:
-                // skriva ut
+                printRegistery(itemList, nrOfProducts);
+                break;
             case 3:
-                // s”ka efter
+                searchMenu(itemList, nrOfProducts);
+                break;
             case 4:
                 // lagersaldot 
             case 5:
@@ -80,6 +54,161 @@ int main()
             case 7:
                 break;
                 // avsluta programmet
+            default:
+                printf("Tillg„ngliga alternativ (1-7)!\n");
+        }
+    }
+}
+
+Warehouse createProduct(char productName[], int productNumber, int productAmount)
+{
+    Warehouse p;
+    strcpy(p.productName, productName);
+    p.productNumber = productNumber;
+    p.productAmount = productAmount;
+    return p;
+}
+
+void registerProduct(Warehouse registry[], int *pNrOfProducts)
+{
+    int productNumber, productAmount, unique = 0;
+    char productName[WORDLENGTH];
+
+    while (productNumber != 0) //&& *pNrOfProducts < MAX)
+    {
+        unique = 0;
+
+        if(*pNrOfProducts >= MAX)
+        {
+            printf("Du har inte mer plats for fler varor\n");
+            break;
+        }
+        printf("Ange varunummer (0 f”r avslut): ");
+        scanf("%d%*c", &productNumber);
+
+        if(productNumber == 0)
+        {
+            break;
+        }
+        
+        for(int i = 0; i < *pNrOfProducts; i++)
+        {
+            if(productNumber == registry[i].productNumber)
+            {
+                printf("Ej unik nummer!\n");
+                unique++;
+                printf("%d", unique);
+            }
+        }
+        if(unique == 0)
+        {
+            printf("Ange namn: ", productName);
+            scanf("%s%*c", productName);
+            printf("Ange saldo: ", productAmount);
+            scanf("%d%*c", &productAmount);
+            
+            registry[*pNrOfProducts] = createProduct(productName, productNumber, productAmount);
+            (*pNrOfProducts)++;
+        }
+        
+    } 
+}
+
+void printProducts(Warehouse p)
+{
+    printf("%03d            %s  %5d\n", p.productNumber, p.productName, p.productAmount);
+} 
+
+void printRegistery(Warehouse registry[], int nrOfProducts)
+{
+    printf("VARUNUMMER      NAMN        LAGERSALDO\n");
+    printf("--------------------------------------\n");
+    for(int i = 0; i < nrOfProducts; i++)
+    {
+        printProducts(registry[i]);
+    }
+}
+
+void searchMenu(Warehouse registry[], int nrOfProducts)
+{
+    int menuChoice, userProductNumber, userProductName, userProductAmount;
+    while(menuChoice != 4)
+    {
+        printf("\nVilken vill du s”ka efter?\n");
+        printf(" 1. Varunummer\n 2. Lagersaldo\n 3. Produktnamn \n 4. Tillbaka till meny ");
+        scanf("%d", &menuChoice);
+
+        switch(menuChoice)
+        {
+            case 1:
+                searchProductNumber(registry, nrOfProducts);
+                break;
+            case 2:
+                searchProductAmount(registry, nrOfProducts);
+                break;
+            case 3:
+                break;
+        }
+    }
+}
+
+
+void searchProductNumber(Warehouse registry[], int nrOfProducts)
+{   
+    int userProductNumber, productNumber, productAmount, digit_NotFound;
+    char productName[WORDLENGTH];
+
+    while(nrOfProducts <= MAX)
+    {
+        digit_NotFound = 0;
+        printf("Ange varunummer: ");
+        scanf("%d", &userProductNumber);
+        if(userProductNumber == 0)
+        {
+            break;
+        }
+        for(int i=0; i != nrOfProducts; i++)
+        {
+            if(userProductNumber == registry[i].productNumber)
+            {
+                printf("Varunummer     Namn     Lagersaldo\n");
+                printf("----------------------------------\n");
+                printf("%03d             %s       %d\n", registry[i].productNumber, registry[i].productName, registry[i].productAmount);
+            }
+            else
+            {
+                digit_NotFound++;
+            }
+        }
+        if(digit_NotFound == nrOfProducts)
+        {
+            printf("Kunde inte hitta produkten, f”rs”k igen\n");
+        }
+    }
+}
+
+void searchProductAmount(Warehouse registry[], int nrOfProducts)
+{
+    int userProductAmount, productNumber, productAmount, digit_NotFound;
+    char productName[WORDLENGTH];
+
+    while(nrOfProducts <= MAX)
+    {
+        digit_NotFound = 0;
+        printf("Ange lagersaldo: ");
+        scanf("%d", &userProductAmount);
+        if(userProductAmount == 0)
+        {
+            break;
+        }
+            printf("Varunummer     Namn     Lagersaldo\n");
+            printf("----------------------------------\n");
+        for(int i=0; i != nrOfProducts; i++)
+        {
+            if(userProductAmount == registry[i].productAmount)
+            {
+                printf("%03d             %s       %d\n", registry[i].productNumber, registry[i].productName, registry[i].productAmount);
+            }
         }
     }
 }
--- LABB/LABB-3/test.c
+++ LABB/LABB-3/test.c
@@ -3,37 +3,42 @@
 #include <stdlib.h>
 #define WORDLENGTH 30
 #define MAX 100
-struct car{
-char model[WORDLENGTH];
-int year;
-int milage;
+struct car
+{
+    char model[WORDLENGTH];
+    int year;
+    int milage;
 };
 typedef struct car Car;
-void printCar(Car c){
-printf("Bil: %s, Arsmodell: %d, mil: %d\n",c.model,c.year,c.milage);
+void printCar(Car c)
+{
+    printf("Bil: %s, Arsmodell: %d, mil: %d\n",c.model,c.year,c.milage);
 }
-Car createCar(char model[],int year, int milage){
-Car c;
-strcpy(c.model,model);
-c.year=year;
-c.milage=milage;
-return c;
+Car createCar(char model[],int year, int milage)
+{
+    Car c;
+    strcpy(c.model,model);
+    c.year=year;
+    c.milage=milage;
+    return c;
 }
-void regCars(Car reg[],int *pNrOfCars){
-char again[WORDLENGTH] = "ja", model[WORDLENGTH];
-int year,milage;
-while(strcmp(again,"ja")==0){ //#include <string.h>
-printf("Ange model:");
-scanf("%s%*c",model);
-printf("Ange year:");
-scanf("%d%*c",&year);
-printf("Ange milage:");
-scanf("%d%*c",&milage);
-reg[*pNrOfCars]=createCar(model,year,milage);
-(*pNrOfCars)++;
-printf("Vill du fortsatta? (ja/nej)");
-scanf("%s%*c",again);
-}
+void regCars(Car reg[],int *pNrOfCars)
+{
+    char again[WORDLENGTH] = "ja", model[WORDLENGTH];
+    int year,milage;
+    while(strcmp(again,"ja")==0)
+    { //#include <string.h>
+        printf("Ange model:");
+        scanf("%s%*c",model);
+        printf("Ange year:");
+        scanf("%d%*c",&year);
+        printf("Ange milage:");
+        scanf("%d%*c",&milage);
+        reg[*pNrOfCars]=createCar(model,year,milage);
+        (*pNrOfCars)++;
+        printf("Vill du fortsatta? (ja/nej)");
+        scanf("%s%*c",again);
+    }
 }
 void printRegister(Car reg[], int nrOfCars)
 {
