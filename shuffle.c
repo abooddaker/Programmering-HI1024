0a9a691562a2ffa4a5efca73031415ab3c7b2ee6
abooddaker <abooddaker@gmail.com>
2022-12-12 16:08:20 +0100

    klj

--- shuffle.c
+++ shuffle.c
@@ -4,9 +4,11 @@
 #define SIZE 6
 
 void shuffle(int cards[], int size);
+void shuffle2(char letters[], int size);
 
 int main()
 {
+    char letters[SIZE] = {'A','A','B','B','C','C'};
     int cards[SIZE];
 
     for (int j = 0; j < SIZE; j++)
@@ -17,15 +19,16 @@ int main()
     srand(time(0));
 
     shuffle(cards, SIZE);
+    shuffle2(letters, SIZE);
     printf("\n");
 
     for (int j = 0; j < SIZE; j++)
     {
-        printf("%d, ", cards[j]);
+        //printf("%d, ", cards[j]);
+        printf("%c, ", letters[j]);
+
     }
     return 0;
-
-
 }
 
 void shuffle(int cards[], int size)
@@ -38,4 +41,15 @@ void shuffle(int cards[], int size)
         cards[num] = temp;
     }
 
+}
+
+void shuffle2(char letters[], int size)
+{
+        for (int i = 0; i < SIZE; i++)  
+    {  
+        int num = rand() % SIZE;
+        int temp = letters[i];
+        letters[i] = letters[num];
+        letters[num] = temp;
+    }
 }
