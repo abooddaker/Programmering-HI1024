723546501e4079085653ca335a76fcd15b70a621
abood <abooddaker@gmail.com>
2023-05-10 23:52:41 +0200

    Updated the switch

--- TentaPlugg/Uppgift2/210312Glosprogram.c
+++ TentaPlugg/Uppgift2/210312Glosprogram.c
@@ -16,11 +16,11 @@ int main(void)
 
         switch(menuChoice)
         {
-            case 1: // test
+            case 1: printf("Case 1\n");
             break;
-            case 2: 
+            case 2: printf("Case 2\n");
             break;
-            case 3: 
+            case 3: printf("Case 3\n");
             break;
 
         }
