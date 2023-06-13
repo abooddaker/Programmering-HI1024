5934ab6d266ddea8d1cb0e9a2a23ccc96ee9ece5
abood <abooddaker@gmail.com>
2023-06-13 23:06:12 +0200

    Update test.c

--- TentaPlugg/Uppgift2/test.c
+++ TentaPlugg/Uppgift2/test.c
@@ -66,6 +66,7 @@ int write_eng(char str[][MAX_LENGTH+1], int n)
     printf("Svenska: ");
     printf("Svenska: ");
     printf("Svenska: ");
+    printf("Svenska: ");
     scanf("%s", str[n]);
     return 0;
 }
